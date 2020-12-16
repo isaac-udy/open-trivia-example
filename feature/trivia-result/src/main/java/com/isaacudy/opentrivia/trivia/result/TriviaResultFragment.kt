d34b4653a99d46db3a6cadcda4d127d1c9a7679b
Isaac <isaac.udy@gmail.com>
2020-12-17 12:32:27 +1300

    Add simple UI to the results screen

--- feature/trivia-result/src/main/java/com/isaacudy/opentrivia/trivia/result/TriviaResultFragment.kt
+++ feature/trivia-result/src/main/java/com/isaacudy/opentrivia/trivia/result/TriviaResultFragment.kt
@@ -1,5 +1,6 @@
 package com.isaacudy.opentrivia.trivia.result
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -11,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import nav.enro.annotations.NavigationDestination
 import nav.enro.core.forward
 import nav.enro.core.getNavigationHandle
+import nav.enro.core.navigationHandle
 import nav.enro.core.replaceRoot
 
 @AndroidEntryPoint
@@ -18,10 +20,34 @@ import nav.enro.core.replaceRoot
 class TriviaResultFragment : Fragment(R.layout.trivia_result_fragment) {
 
     private val binding by viewBinding<TriviaResultFragmentBinding>()
+    private val navigation by navigationHandle<TriviaResultScreen>()
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.goTolauncher.setOnClickListener {
-            getNavigationHandle<TriviaResultScreen>().replaceRoot(TriviaLauncherScreen())
+        val correctQuestions = navigation.key.answers.count { it }
+        val totalQuestions = navigation.key.answers.size
+        val percentCorrect =
+            correctQuestions.times(100).toDouble() / totalQuestions.times(100).toDouble()
+
+        val questionText = "You scored $correctQuestions out of $totalQuestions\n\n"
+
+        binding.message.text = questionText + when {
+            percentCorrect < 33 -> {
+                 "Whew, that wasn't so great..."
+            }
+            percentCorrect < 66 -> {
+                "That wasn't too bad, but you can do better!"
+            }
+            percentCorrect < 99 -> {
+                "Well done, that was pretty good!"
+            }
+            else -> {
+                "Wow! I'm impressed!"
+            }
+        }
+
+        binding.homeButton.setOnClickListener {
+            navigation.replaceRoot(TriviaLauncherScreen())
         }
     }
 }
